50ea13c103e8c46d79f2c5bc633e4a7f93d34b6b
Dima Vorobev <dimjkee@icloud.com>
2023-02-16 10:17:47 +0300

    add notRemoved

--- src/main/java/ru/netology/model/Post.java
+++ src/main/java/ru/netology/model/Post.java
@@ -3,6 +3,7 @@ package ru.netology.model;
 public class Post {
     private long id;
     private String content;
+    private boolean isRemove;
 
     public Post() {
     }
@@ -10,6 +11,7 @@ public class Post {
     public Post(long id, String content) {
         this.id = id;
         this.content = content;
+        this.isRemove = false;
     }
 
     public long getId() {
@@ -26,5 +28,13 @@ public class Post {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public void setRemove(boolean setFlagToRemove) {
+        this.isRemove = setFlagToRemove;
+    }
+
+    public boolean getIsRemove() {
+        return isRemove;
     }
 }
--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,46 +1,60 @@
 package ru.netology.repository;
 
 import org.springframework.stereotype.Repository;
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @Repository
 public class PostRepository {
-    public final AtomicLong postID = new AtomicLong(0);
+    public final AtomicLong postID = new AtomicLong(1);
     private final ConcurrentMap<Long, Post> postsMap = new ConcurrentHashMap<>();
 
     public List<Post> all() {
-        return new ArrayList<>(postsMap.values());
+        return postsMap.values().stream()
+                .filter(post -> !post.getIsRemove())
+                .collect(Collectors.toList());
     }
 
     public Optional<Post> getById(long id) {
-        return Optional.of(postsMap.get(id));
+        Optional<Post> optionalPost = postsMap.values().stream()
+                .filter(x -> x.getId() == id && !x.getIsRemove())
+                .findFirst();
+        if (optionalPost.isPresent()) {
+            return optionalPost;
+        } else {
+            throw new NotFoundException();
+        }
     }
 
     public Post save(Post post) {
-        if (post.getId() != 0 && postsMap.containsKey(post.getId())) {
-            postsMap.replace(post.getId(), post);
-        } else if (post.getId() == 0) {
-            long newPostId = postID.incrementAndGet();
-            post.setId(newPostId);
-            postsMap.put(newPostId, post);
+        if (post.getId() == 0) {
+            postsMap.put(postID.get(), new Post(postID.get(), post.getContent()));
+            postID.incrementAndGet();
+            return post;
+        } else {
+            if (post.getId() != 0 && postsMap.containsKey(post.getId()) && !postsMap.get(post.getId()).getIsRemove()) {
+                postsMap.replace(post.getId(), post);
+                return post;
+            } else {
+                throw new NotFoundException();
+            }
         }
-        return post;
     }
 
     public void removeById(long id) {
-        if (postsMap.containsKey(id)) {
-            postsMap.remove(id);
+        Optional<Map.Entry<Long, Post>> optionalEntry = postsMap.entrySet().stream()
+                .filter(x -> x.getKey() == id && !x.getValue().getIsRemove())
+                .findFirst();
+        if (optionalEntry.isPresent()) {
+            optionalEntry.get().getValue().setRemove(true);
         } else {
-            System.out.println("Такого id не существует!");
+            throw new NotFoundException();
         }
     }
 }
